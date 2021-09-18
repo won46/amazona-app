d645a6b7920ca8d5a670108c26a70638345ff550
unknown <wonn46@gmail.com>
2021-09-18 20:14:12 +0700

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,48 @@
+
+import data from './data';
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">amazona</a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {
+            data.products.map(product => (
+              <div key={product._id} className="card">
+                <a href={`/product/${product._id}`} >
+                  <img className="medium" src={product.image} alt="produuct" />
+                </a>
+                <div className="card-body">
+                  <a href={`/product/${product._id}`} >
+                    <h2>{product.name}</h2>
+                  </a>
+                  <div className="rating">
+                    <span><i className="fa fa-star"></i></span>
+                    <span><i className="fa fa-star"></i></span>
+                    <span><i className="fa fa-star"></i></span>
+                    <span><i className="fa fa-star"></i></span>
+                    <span><i className="fa fa-star"></i></span>
+                  </div>
+                  <div className="price">${product.price}</div>
+                </div>
+              </div>
+            ))
+          }
+
+        </div>
+      </main >
+      <footer className="row center">All Right Reserved</footer>
+    </div >
+
+  );
+}
+
+export default App;
